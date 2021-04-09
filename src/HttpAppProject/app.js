caf8dd2e8296f4a16fde96255ec8e1ffc28f7ef6
peoplemaner <peoplemaner@gmail.com>
2021-04-09 12:00:27 +0900

    httpAppServer

---
+++ src/HttpAppProject/app.js
@@ -0,0 +1,48 @@
+/*
+    REST API Server for KSVR
+*/
+const express = require('express');
+const app = express();
+const conf = require("./package.json");
+
+app.use(express.json());
+
+process.on('uncaughtException', function (err) {
+    logger.error("unhandled exception : " + err + "\n" + err.stack);
+});
+
+const async = require('async');
+//!< DB 풀 생성.
+const mysql = require('mysql');
+try{
+	const DB = mysql.createPool(conf.db);
+}catch(e){
+	console.log(e);
+}
+
+app.get('/', function(req, res) {
+    console.log(req.query);
+    res.send("Restful API GET...!");
+});
+
+app.get('/check', function(req, res) {
+    console.log(req.query);
+    res.send("Restful API GET...!");
+});
+
+app.post('/', function(req, res) {
+    console.log(req.body);
+    res.send("Restful API POST...!");
+});
+
+app.listen(conf.server.port, function () {
+    console.log('START SERVER...! ' + conf.server.port);
+    /*DB.query("SELECT * FROM ACCOUNT", function(error, result){
+        if(error){
+            console.log(error);
+        } else {
+            console.log(result[0]);
+        }
+
+    });*/
+});ß
