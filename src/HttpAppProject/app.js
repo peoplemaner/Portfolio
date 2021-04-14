842b27a3dbb80cc8cd1003a69fa00edf658be8ad
DESKTOP-0EVLT32\USER <peoplemaner@gmail.com>
2021-04-14 16:46:09 +0900

    로그인, 회원가입, 방송방 생성/조회, app.js log middleware 추가

--- src/HttpAppProject/app.js
+++ src/HttpAppProject/app.js
@@ -2,12 +2,20 @@
     REST API Server
 */
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const conf = require("./package.json");
 const user = require('./routes/user');
+const broadcast = require('./routes/broadcast');
 
 app.use(express.json());
 app.use(express.urlencoded( {extended: false}));
+app.use(cors());
+
+app.use((req, res, next) => {
+    console.log(`url: ${req.headers.host}${req.path} / query: ${JSON.stringify(req.query)} / body: ${JSON.stringify(req.body)}`);
+    next();
+});
 
 process.on('uncaughtException', function (err) {
     console.log("unhandled exception : " + err + "\n" + err.stack);
@@ -19,20 +27,24 @@ const mysql = require('mysql');
 try { global.DB = mysql.createPool(conf.db); } catch (e) { console.log(e); }
 
 app.get('/', function(req, res) {
-    console.log(req.query);
     res.send("Restful API GET...!");
 });
 
 app.get('/check', function(req, res) {
-    console.log(req.query);
     res.send("Restful API GET...!");
 });
 
-// 회원조회
-app.get('/users', (req, res) =>  { user.login(req, res); });
+// 로그인
+app.post('/login', (req, res) =>  { user.login(req, res); });
 
 // 회원가입
 app.post('/users', (req, res) => { user.createUser(req, res); });
+
+// 방송방 조회
+app.get('/broadcasts', (req, res) =>  { broadcast.getBroadCasts(req, res); });
+
+// 방송방 생성
+app.post('/broadcasts', (req, res) => { broadcast.createBroadcast(req, res); });
 
 app.listen(conf.server.port, () => {
     console.log('START SERVER...! ' + conf.server.port);
